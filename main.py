2fe1e4081602fbb6d38a4321dcc6a7daa8b09eaa
unknown <rikbiswas031@gmail.com>
2026-05-04 02:51:24 +0530

    Describe what you changed

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ class Post(BaseModel):
     published: bool = True   
     rating:Optional[int] = None
 
+
+
 my_posts = [{"title": "post 1", "content": "content of post 1" ,"id": 1}, {"title": "post 2", "content": "content of post 2",  "id": 2},
-            {"title": "post 3", "content": "content of post 3",  "id": 3}]
+            {"title": "post 3", "content": "content of post 3",  "id": 3},
+            {"title": "post 4", "content": "content of post 4",  "id": 4}]
 
 
 
@@ -21,7 +24,11 @@ def find_post(id):
     for p in my_posts:
         if p['id'] == id:
             return p
-        
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i       
 
 @app.get("/")
 def get_user():
@@ -31,7 +38,7 @@ def get_user():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/createpost")
+@app.post("/createpost", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
    post_dict = post.dict()
    post_dict["id"] = randrange(0, 1000000)
@@ -49,4 +56,25 @@ def get_post(id: int , response: Response):
          
    print = (post)
    return {"post_detail " :post}
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post): 
+    print(post)
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
+
 
